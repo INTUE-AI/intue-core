69c550ee16c377d72805cd682efede2faf2aebb1
INTUE-AI <intueai@gmail.com>
2025-04-26 16:10:25 -0700

    Initial implementation of INTUE Core
    INTUE - The intelligence layer for AI agents, now open-sourced after years of internal development. Previously powering proprietary trading strategies, INTUE has been rebuilt from the ground up with a modular architecture focused on crypto sentiment analysis and cross-ecosystem correlations. This initial release includes our core utilities and LunarCrush integration.

---
+++ src/index copy.js
@@ -0,0 +1,5 @@
+const Cache = require('./utils/cache');
+
+module.exports = {
+  Cache
+};
---
+++ src/types/index.js
@@ -0,0 +1,16 @@
+/**
+ * @typedef {Object} CacheOptions
+ * @property {number} [ttl=300000] - Time to live in milliseconds (default: 5 minutes)
+ */
+
+/**
+ * @typedef {Object} DataPoint
+ * @property {string} asset - Asset symbol
+ * @property {number} sentiment - Sentiment score
+ * @property {number} socialVolume - Social volume count
+ * @property {number} engagement - Engagement score
+ * @property {string} timeframe - Analysis timeframe
+ * @property {string} source - Data source
+ */
+
+module.exports = {};
